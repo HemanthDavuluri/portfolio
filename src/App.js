a96c35f8bf3fe92eb126f4190a621d29f695d591
Restyled.io <commits@restyled.io>
2022-04-06 11:23:05 +0000

    Restyled by clang-format

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import { useEffect } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { setPreChecking, setUnderConstruction } from "./redux/configSlice";
-import { useSelector, useDispatch } from "react-redux";
-import { db } from "./util/Firebase";
-import { getDoc, doc } from "firebase/firestore/lite";
-import UnderConstruction from "./pages/Under Construction/UnderConstruction";
-import CircularProgress from "@mui/material/CircularProgress";
+
 import Box from "@mui/material/Box";
+import CircularProgress from "@mui/material/CircularProgress";
+import {doc, getDoc} from "firebase/firestore/lite";
+import {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+
+import logo from "./logo.svg";
+import UnderConstruction from "./pages/Under Construction/UnderConstruction";
+import {setPreChecking, setUnderConstruction} from "./redux/configSlice";
+import {db} from "./util/Firebase";
 
 function App() {
   const dispatch = useDispatch();
   const pre_checking = useSelector((state) => state.config.pre_checking);
-  const under_construction = useSelector(
-    (state) => state.config.under_construction
-  );
+  const under_construction =
+      useSelector((state) => state.config.under_construction);
 
   useEffect(() => {
     const getConfig = async () => {
@@ -23,25 +24,19 @@ function App() {
       if (!docRef.exists) {
         window.alert("error");
       } else {
-        dispatch(setPreChecking({ pre_checking: false }));
-        dispatch(
-          setUnderConstruction({
-            under_construction: docRef.data().under_construction,
-          })
-        );
+        dispatch(setPreChecking({pre_checking : false}));
+        dispatch(setUnderConstruction({
+          under_construction : docRef.data().under_construction,
+        }));
       }
     };
     getConfig();
-  }, [dispatch]);
+  }, [ dispatch ]);
 
   if (pre_checking === true) {
-    return (
-      <div className="App">
-        <Box sx={{ display: "flex" }}>
-          <CircularProgress />
-        </Box>
-      </div>
-    );
+    return (<div className = "App"><Box sx = {{ display: "flex" }}>
+            <CircularProgress /></Box>
+      </div>);
   } else if (under_construction === true) {
     return <UnderConstruction />;
   } else {
@@ -53,15 +48,12 @@ function App() {
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            welcome to portfolio
-          </a>
-        </header>
-      </div>
+    className = "App-link"
+    href = "https://reactjs.org"
+    target = "_blank"
+    rel = "noopener noreferrer" > welcome to portfolio</a>
+        </header><
+          /div>
     );
   }
 }
