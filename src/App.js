6333d9c499be5f479344104d3600fff96417b9b1
Davuluri Hemanth Chowdary <hemanth.davuluri@minutestoseconds.com>
2022-04-06 16:36:51 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
+import { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { setPreChecking, setUnderConstruction } from "./redux/configSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { db } from "./util/Firebase";
+import { getDoc, doc } from "firebase/firestore/lite";
+import UnderConstruction from "./pages/Under Construction/UnderConstruction";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          welcome to portfolio
-        </a>
-      </header>
-    </div>
+  const dispatch = useDispatch();
+  const pre_checking = useSelector((state) => state.config.pre_checking);
+  const under_construction = useSelector(
+    (state) => state.config.under_construction
   );
+
+  useEffect(() => {
+    const getConfig = async () => {
+      const Doc = doc(db, "portfolio-config/portfolio-config");
+      const docRef = await getDoc(Doc);
+      if (!docRef.exists) {
+        window.alert("error");
+      } else {
+        dispatch(setPreChecking({ pre_checking: false }));
+        dispatch(
+          setUnderConstruction({
+            under_construction: docRef.data().under_construction,
+          })
+        );
+      }
+    };
+    getConfig();
+  }, [dispatch]);
+
+  if (pre_checking === true) {
+    return (
+      <div className="App">
+        <p>Loading configs</p>
+      </div>
+    );
+  } else if (under_construction === true) {
+    return <UnderConstruction />;
+  } else {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            welcome to portfolio
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/Under Construction/UnderConstruction.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./underConstruction.css";
+
+function underConstruction() {
+  return (
+    <div className="under-construction">
+      <div className="top">
+        <h3>Davuluri Hemanth Chowdary</h3>
+      </div>
+      <div className="middle">
+        <h1>Work in progress</h1>
+      </div>
+      <div className="bottom_1">
+        <h3>
+          My portfolio will be launched very soon <br /> &nbsp; &nbsp; &nbsp;
+          come back and check it out later
+        </h3>
+      </div>
+      {/* <div className="footer">
+        <p>instagram</p>
+      </div> */}
+    </div>
+  );
+}
+
+export default underConstruction;
